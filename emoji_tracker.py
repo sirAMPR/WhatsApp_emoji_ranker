26bffedcd03aad87028f9da83bd97021468d3292
Drew Radcliff <sirampr@gmail.com>
2019-11-26 11:18:36 -0500

    modify regex and clean emoji list

--- emoji_tracker.py
+++ emoji_tracker.py
@@ -15,24 +15,34 @@ print('Backend is now ' + matplotlib.get_backend())
 import matplotlib.pyplot as plt
 import re
 from matplotlib.font_manager import FontProperties
-#import numpy as np
+# import numpy as np
 
-f = open("discussion.txt","r")
+f = open("_chat.txt","r")
 content = f.readlines()
 lVasi=[]
 lLuc=[]
 for x in content:
-    if(x.find("Name1") != -1):
+    if(x.find("Jessicarr") != -1):
         lVasi.append(x)
-    elif(x.find("Name2") != -1):
+    elif(x.find("Drew Radcliff") != -1):
         lLuc.append(x)
 
 strVasi = ''.join(lVasi)
 strLuc = ''.join(lLuc)
-exp = re.compile(u'([\U00002600-\U000027BF])|([\U0001f300-\U0001f64F])|([\U0001f680-\U0001f6FF])')
+# exp = re.compile(u'([\U00002600-\U000027BF])|([\U0001f300-\U0001f64F])|([\U0001f680-\U0001f6FF])')
+# exp = re.compile(u'(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])')
+exp = re.compile('[\U00010000-\U0010ffff]', flags=re.UNICODE)
 emoji_list_Vasi_raw = exp.findall(strVasi)
 emoji_list_Luc_raw = exp.findall(strLuc)
 
+for e in emoji_list_Vasi_raw:
+    if e == '🏻' or e == '🏼':
+        emoji_list_Vasi_raw.remove(e)
+
+for e in emoji_list_Luc_raw:
+    if e == '🏻' or e == '🏼':
+        emoji_list_Luc_raw.remove(e)
+        
 emoji_list_Vasi = []
 emoji_list_Luc = []
 for l in emoji_list_Vasi_raw:
@@ -41,9 +51,9 @@ for l in emoji_list_Vasi_raw:
 for l in emoji_list_Luc_raw:
     emoji_list_Luc.append(''.join(l))
 
-#print(emoji_list_Vasi)
-#print("\n")
-#print(emoji_list_Luc)
+# print(emoji_list_Vasi)
+# print("\n")
+# print(emoji_list_Luc)
 
 emoji_freq_Vasi = {}
 emoji_freq_Luc = {}
@@ -70,8 +80,10 @@ for key in emoji_freq_Luc.keys():
 emoji_freq_disp_Vasi = dict(sorted([(k,v) for k, v in emoji_freq_Vasi.items()], key = lambda x: x[1], reverse=True)[0:7])
 emoji_freq_disp_Luc = dict(sorted([(k,v) for k, v in emoji_freq_Luc.items()], key = lambda x: x[1], reverse=True)[0:7])
 
-print(emoji_freq_disp_Vasi)
-print(emoji_freq_disp_Luc)   
+# print("J")
+# print(emoji_freq_disp_Vasi)
+# print("D")
+# print(emoji_freq_disp_Luc)   
 
 
 prop = FontProperties(fname='/System/Library/Fonts/Apple Color Emoji.ttc')
@@ -82,14 +94,11 @@ vasi.set_xticklabels(emoji_freq_disp_Vasi.keys(),fontproperties=prop,size=30)
 luc.set_xticklabels(emoji_freq_disp_Luc.keys(),fontproperties=prop,size=30)
 vasi.set_ylabel("Relative number of occurences (%)")
 luc.set_ylabel("Relative number of occurences (%)")
-luc.set_title("Him")
-vasi.set_title("Her")
+luc.set_title("D")
+vasi.set_title("J")
 luc.bar(emoji_freq_disp_Luc.keys(), emoji_freq_disp_Luc.values(), color='blue')
 vasi.bar(emoji_freq_disp_Vasi.keys(), emoji_freq_disp_Vasi.values(), color='pink')
-
-
-
-        
+plt.show()
         
 f.close()
 
